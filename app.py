25eb0bbaa59b19d4550552174dd2ccc61bf89463
Luis Vinicius <luislauriano@outlook.com.br>
2022-07-29 18:56:04 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,26 +11,14 @@ def main():
     st.title("Aplicação de Machine Learning para prever possíveis resultados de jogos da copa 2022")
      
     html_temp2 = """
-		<div style="background-color:#FF0031;padding:-120px;border-radius:-120px ; height:200px">
-		<h2 style="color:white;text-align:center;">Copa do mundo 2022 </h2>
-        <h1 style="color:white;text-align:center;">Escolha dois times e veja o possível resultado para um jogo da copa do mundo de 2022</h1>
+		<div style="background-color:white;padding:-120px;border-radius:-120px ; height:200px">
+		<h2 style="color:#FF0031;text-align:center;">Copa do mundo 2022 </h2>
+        <h1 style="color:#FF0031;text-align:center;">Escolha dois times e veja o possível resultado para um jogo da copa do mundo de 2022</h1>
 		</div>
 		"""
-    # a simple html code for heading which is in blue color and we can even use "st.write()" also ut for back ground color i used this HTML ..... 
-    #  to render this we use ...
-    components.html(html_temp2)
-    # components.html() will render the render the 
-
-    #components.html("""
-                #<img src="http://sportinsider.com.br/wp-content/uploads/2022/05/copa-do-mundo-do-catar-2022-1.jpg" width="1500px" height="1050px">
-                
-                #""")
-
-
-    # this is to insert the image the in the wed app simple <imag/> tag in HTML
     
-    #now lets get the test input from the user by wed app 
-    # for this we can use "st.text_input()" which allows use to get the input from the user 
+    components.html(html_temp2)
+    
     
   
     st.subheader('Prever partidas da fase de grupo')
@@ -52,13 +40,14 @@ def main():
     result_grupo =""
 
 
-    # done we got all the user inputs to predict and we need a button like a predict button we do that by "st.button()"
-    # after hitting the button the prediction process will go on and then we print the success message by "st.success()"
+   
 
     if st.button("Prever resultado de partida da fase de grupos"):
         if ((grupo_time1 or grupo_time2) != 'Escolha um time'):
-            result_grupo = prep.predict_grupo(grupo_time1, grupo_time2)
-            st.success('Possível esultado da partida: {}'.format(result_grupo))
+            result_grupo = (prep.predict_grupo(grupo_time1, grupo_time2)[0:3])
+            chances_vitor_empate_derrota = str(prep.predict_grupo(grupo_time1, grupo_time2)[3:])
+            st.success('Possível resultado da partida: {}'.format(result_grupo))
+            st.success('Chances de vitória, empate e derrota: {}'.format(chances_vitor_empate_derrota))
          
        
         else:
@@ -68,8 +57,6 @@ def main():
 
 
     #previsão para partidas de eliminatórias 
-
-    
     st.subheader('Prever partidas da fase eliminatória')
 
     eliminatoria_time1 = st.selectbox('Time 1 (Fase eliminatória)', ['Escolha um time','Qatar', 'Senegal', 'Netherlands', 'Ecuador', 'England', 'Iran',
@@ -89,28 +76,41 @@ def main():
     result_eliminatoria=""
 
 
-    # done we got all the user inputs to predict and we need a button like a predict button we do that by "st.button()"
-    # after hitting the button the prediction process will go on and then we print the success message by "st.success()"
 
     if st.button("Prever resultado de partida eliminatória"):
         if ((eliminatoria_time1 or eliminatoria_time2) != 'Escolha um time'):
-            result_eliminatoria = str(prep.predict_eliminatoria(eliminatoria_time1, eliminatoria_time2))
+            result_eliminatoria = str(prep.predict_eliminatoria(eliminatoria_time1, eliminatoria_time2)[0:3])
+            chances_vitoria_empate_derrota = str(prep.predict_eliminatoria(eliminatoria_time1, eliminatoria_time2)[3:])
             st.success('Possível resultado da partida: {}'.format(result_eliminatoria))
+            st.success('Chances de vitória, empate e derrota: {}'.format(chances_vitoria_empate_derrota))
        
             
         else:
             st.warning('Você precisa escolher um time')
-            
-    # one more button saying About ...
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
-    
-    st.sidebar.info("Essa aplicação de machine learning está sendo alimentada por um modelo que é capaz de prever um possível resultado para jogos da copa do mundo")
-    st.sidebar.info("O modelo foi construído a partir de dados da FIFA e resultados de copas anteriores")
-    st.sidebar.info("Os resultados de uma mesma simulação podem acabar sendo diferentes, tendo em vista que o resultado é um cálculo de probabilidade ")
-    st.sidebar.info("O resultado para uma partida pode mudar dependendo se for uma partida da fase de grupos ou fase eliminatória, por isso existem duas opções")
 
+
+    #Simulação dos jogos
+    st.subheader('Simulação da fase de grupos e fase mata-mata')
+    st.write('Ao selecionar um simulador você irá obter uma simulação de como possivelmente irá terminar a fase de grupos e a fase mata-mata com base nos resultados possíveis que o modelo preveu.')
+    st.write('O simulador da fase mata-mata acaba demorando um pouco para retornar o resultado por conta que a imagem leva um tempo para ser carregada. A imagem pode ser ampliada clicando no ícone de expansão ao lado')
+    col1, col2, col3 = st.columns(3)
+    if col2.button('Simular resultado da fase de grupos'):
+       for group_rank in prep.groups_ranking:
+           st.write(group_rank)
+    
+    col4, col5, col6 = st.columns(3)
+    if col5.button('Simular resultado da fase mata-mata'):
+        st.pyplot(prep.fig)
+
+         
+       
+    
+    st.sidebar.info('Autor: Luis Vinicius (el tito) - @l.uisvinicius')
+    st.sidebar.info("Essa aplicação de machine learning foi construída a partir de um modelo linear generalizado com base na regressão de Poisson, o objetivo do modelo é prever um possível resultado para jogos da copa do mundo, utilizando como target a quantidade de gols em uma partida")
+    st.sidebar.info("O modelo foi construído a partir de dados do ranking de seleções e partidas internacionais da FIFA")
+    st.sidebar.info("O resultado da partida é feito a partir da escolha do resultado mais provável, diante da probabilidade de cada resultado possível (vitória, empate e derrota) que foi calculado em uma função com base em todas as probabilidades de resultados possíveis. Por esse motivo, o resultado da partida pode acabar sendo diferente se testado outra vez. Para ficar mais claro, podemos imaginar a cena do doutor estranho no filme guerra infinita onde ele encontra um único resultado positivo para eles vencerem a guerra diante de todos os resultados possiveis finais que a guerra contra thanos poderia ter.")
+    st.sidebar.info("Para uma partida eliminatória o resultado de um jogo não pode ser empate, então existe uma opção apenas para ser calculado o possível resultado de partidas eliminatórias")
+    
     
 if __name__=='__main__':
     main()
--- prep_data.py
+++ prep_data.py
@@ -3,46 +3,46 @@ import pickle as pkl
 import pandas as pd
 import numpy as np
 from scipy.stats import poisson
+import itertools
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 
-
-#Load the saved model
 model=pkl.load(open("poisson_model.pkl","rb"))
 
 def predict_grupo(time1, time2):
 
     def get_proba_match(foot_model, team1, team2, max_goals=10):
-        # Get the average goal for each team
+        
         t1_goals_avg = foot_model.predict(pd.DataFrame(data={'team': team1, 'opponent': team2}, index=[1])).values[0]
         t2_goals_avg = foot_model.predict(pd.DataFrame(data={'team': team2, 'opponent': team1}, index=[1])).values[0]
         
-        # Get probability of all possible score for each team
+      
         team_pred = [[poisson.pmf(i, team_avg) for i in range(0, max_goals+1)] for team_avg in [t1_goals_avg, t2_goals_avg]]
         
         #todos as probabilidades de resultados possiveis
         match = np.outer(np.array(team_pred[0]), np.array(team_pred[1]))
         
-        # Get the proba for each possible outcome
+        
         t1_wins = np.sum(np.tril(match, -1))
         draw = np.sum(np.diag(match))
         t2_wins = np.sum(np.triu(match, 1))
         result_proba = [t1_wins, draw, t2_wins]
         
-        # Adjust the proba to sum to one
+        
         result_proba =  np.array(result_proba)/ np.array(result_proba).sum(axis=0,keepdims=1)
         team_pred[0] = np.array(team_pred[0])/np.array(team_pred[0]).sum(axis=0,keepdims=1)
         team_pred[1] = np.array(team_pred[1])/np.array(team_pred[1]).sum(axis=0,keepdims=1)
         return result_proba, [np.array(team_pred[0]), np.array(team_pred[1])], match
     
     def get_match_result(foot_model, team1, team2, elimination=False, max_draw=50, max_goals=10):
-        # Get the proba
+       
         proba,score_proba, match = get_proba_match(foot_model, team1, team2, max_goals)
         
-        # Get the result, if it's an elimination game we have to be sure the result is not draw
+       
         results = pd.Series([np.random.choice([team1, 'draw', team2], p=proba) for i in range(0,max_draw)]).value_counts()
         result = results.index[0] if not elimination or (elimination and results.index[0] != 'draw') else results.index[1]
         
-        # If the result is not a draw game then we calculate the score of the winner from 1 to the max_goals 
-        # and the score of the looser from 0 to the score of the winner
+     
         if (result != 'draw'): 
             i_win, i_loose = (0,1) if result == team1 else (1,0)
             score_proba[i_win] = score_proba[i_win][1:]/score_proba[i_win][1:].sum(axis=0,keepdims=1)
@@ -54,8 +54,7 @@ def predict_grupo(time1, time2):
             
             score = [winner_score, looser_score]
             
-        
-        # If it's a draw then we calculate a score and repeat it twice
+     
         else:
             score = np.repeat(pd.Series([np.random.choice(range(0, max_goals+1), p=score_proba[0]) for i in range(0,max_draw)]).value_counts().index[0],2)
         looser = team2 if result == team1 else team1 if result != 'draw' else 'draw'
@@ -74,7 +73,6 @@ def predict_grupo(time1, time2):
         
 
     prediction= get_match_result(model, time1, time2, max_goals=10)
-    #print(prediction)
     return prediction
 
 
@@ -83,23 +81,23 @@ def predict_grupo(time1, time2):
 def predict_eliminatoria(time1_eliminatoria, time2_eliminatoria):
 
     def get_proba_match(foot_model, team1, team2, max_goals=10):
-        # Get the average goal for each team
+      
         t1_goals_avg = foot_model.predict(pd.DataFrame(data={'team': team1, 'opponent': team2}, index=[1])).values[0]
         t2_goals_avg = foot_model.predict(pd.DataFrame(data={'team': team2, 'opponent': team1}, index=[1])).values[0]
         
-        # Get probability of all possible score for each team
+        
         team_pred = [[poisson.pmf(i, team_avg) for i in range(0, max_goals+1)] for team_avg in [t1_goals_avg, t2_goals_avg]]
         
         #todos as probabilidades de resultados possiveis
         match = np.outer(np.array(team_pred[0]), np.array(team_pred[1]))
         
-        # Get the proba for each possible outcome
+      
         t1_wins = np.sum(np.tril(match, -1))
         draw = np.sum(np.diag(match))
         t2_wins = np.sum(np.triu(match, 1))
         result_proba = [t1_wins, draw, t2_wins]
         
-        # Adjust the proba to sum to one
+      
         result_proba =  np.array(result_proba)/ np.array(result_proba).sum(axis=0,keepdims=1)
         team_pred[0] = np.array(team_pred[0])/np.array(team_pred[0]).sum(axis=0,keepdims=1)
         team_pred[1] = np.array(team_pred[1])/np.array(team_pred[1]).sum(axis=0,keepdims=1)
@@ -107,15 +105,14 @@ def predict_eliminatoria(time1_eliminatoria, time2_eliminatoria):
     
     
     def get_match_result(foot_model, team1, team2, elimination=False, max_draw=50, max_goals=10):
-        # Get the proba
+
         proba,score_proba, match = get_proba_match(foot_model, team1, team2, max_goals)
         
-        # Get the result, if it's an elimination game we have to be sure the result is not draw
+       
         results = pd.Series([np.random.choice([team1, 'draw', team2], p=proba) for i in range(0,max_draw)]).value_counts()
         result = results.index[0] if not elimination or (elimination and results.index[0] != 'draw') else results.index[1]
         
-        # If the result is not a draw game then we calculate the score of the winner from 1 to the max_goals 
-        # and the score of the looser from 0 to the score of the winner
+        
         if (result != 'draw'): 
             i_win, i_loose = (0,1) if result == team1 else (1,0)
             score_proba[i_win] = score_proba[i_win][1:]/score_proba[i_win][1:].sum(axis=0,keepdims=1)
@@ -128,7 +125,7 @@ def predict_eliminatoria(time1_eliminatoria, time2_eliminatoria):
             score = [winner_score, looser_score]
             
         
-        # If it's a draw then we calculate a score and repeat it twice
+        
         else:
             score = np.repeat(pd.Series([np.random.choice(range(0, max_goals+1), p=score_proba[0]) for i in range(0,max_draw)]).value_counts().index[0],2)
         looser = team2 if result == team1 else team1 if result != 'draw' else 'draw'
@@ -147,6 +144,162 @@ def predict_eliminatoria(time1_eliminatoria, time2_eliminatoria):
     
 
     prediction= get_match_result(model, time1_eliminatoria, time2_eliminatoria, max_goals=10, elimination=True)
-    #print(prediction)
     return prediction
 
+
+#Funções para simular os jogos
+
+groupA = ['Qatar', 'Senegal', 'Netherlands', 'Ecuador']
+groupB = ['England', 'Iran','United States', 'Wales']
+groupC = ['Argentina', 'Saudi Arabia', 'Mexico', 'Poland']
+groupD = ['France', 'Australia', 'Denmark', 'Tunisia']
+groupE = ['Spain', 'Costa Rica','Germany', 'Japan']
+groupF = ['Belgium', 'Canada', 'Morocco', 'Croatia']
+groupG = ['Brazil', 'Serbia', 'Switzerland', 'Cameroon']
+groupH = ['Portugal', 'Ghana','Uruguay', 'Korea Republic']
+groups = [groupA, groupB, groupC, groupD, groupE, groupF, groupG, groupH]
+
+def get_proba_match(foot_model, team1, team2, max_goals=10):
+        
+        t1_goals_avg = foot_model.predict(pd.DataFrame(data={'team': team1, 'opponent': team2}, index=[1])).values[0]
+        t2_goals_avg = foot_model.predict(pd.DataFrame(data={'team': team2, 'opponent': team1}, index=[1])).values[0]
+        
+      
+        team_pred = [[poisson.pmf(i, team_avg) for i in range(0, max_goals+1)] for team_avg in [t1_goals_avg, t2_goals_avg]]
+        
+        #todos as probabilidades de resultados possiveis
+        match = np.outer(np.array(team_pred[0]), np.array(team_pred[1]))
+        
+        
+        t1_wins = np.sum(np.tril(match, -1))
+        draw = np.sum(np.diag(match))
+        t2_wins = np.sum(np.triu(match, 1))
+        result_proba = [t1_wins, draw, t2_wins]
+        
+        
+        result_proba =  np.array(result_proba)/ np.array(result_proba).sum(axis=0,keepdims=1)
+        team_pred[0] = np.array(team_pred[0])/np.array(team_pred[0]).sum(axis=0,keepdims=1)
+        team_pred[1] = np.array(team_pred[1])/np.array(team_pred[1]).sum(axis=0,keepdims=1)
+        return result_proba, [np.array(team_pred[0]), np.array(team_pred[1])], match
+
+def get_match_result(foot_model, team1, team2, elimination=False, max_draw=50, max_goals=10):
+       
+        proba,score_proba, match = get_proba_match(foot_model, team1, team2, max_goals)
+        
+       
+        results = pd.Series([np.random.choice([team1, 'draw', team2], p=proba) for i in range(0,max_draw)]).value_counts()
+        result = results.index[0] if not elimination or (elimination and results.index[0] != 'draw') else results.index[1]
+        
+     
+        if (result != 'draw'): 
+            i_win, i_loose = (0,1) if result == team1 else (1,0)
+            score_proba[i_win] = score_proba[i_win][1:]/score_proba[i_win][1:].sum(axis=0,keepdims=1)
+            winner_score = pd.Series([np.random.choice(range(1, max_goals+1), p=score_proba[i_win]) for i in range(0,max_draw)]).value_counts().index[0]
+            
+            score_proba[i_loose] = score_proba[i_loose][:winner_score]/score_proba[i_loose][:winner_score].sum(axis=0,keepdims=1)
+            looser_score = pd.Series([np.random.choice(range(0, winner_score), p=score_proba[i_loose]) for i in range(0,max_draw)]).value_counts().index[0]
+            
+            
+            score = [winner_score, looser_score]
+            
+     
+        else:
+            score = np.repeat(pd.Series([np.random.choice(range(0, max_goals+1), p=score_proba[0]) for i in range(0,max_draw)]).value_counts().index[0],2)
+        looser = team2 if result == team1 else team1 if result != 'draw' else 'draw'
+        
+        
+        t1_wins = round(np.sum(np.tril(match, -1)),2)
+        draw = round(np.sum(np.diag(match)),2)
+        t2_wins = round(np.sum(np.triu(match, 1)),2)
+            
+        porcent = (f'Chances de vitória {team1}: {t1_wins}%, chances de empate: {draw}%, chances de vitória {team2}: {t2_wins}%')
+            
+        
+        return result, looser, score, porcent
+        
+#Jogos da fase de grupos
+
+def get_group_result(foot_model, group):
+    ranking = pd.DataFrame({'points':[0,0,0,0], 'diff':[0,0,0,0], 'goals':[0,0,0,0]}, index=group)
+    for team1, team2 in itertools.combinations(group, 2):
+        result, looser, score, porcent = get_match_result(foot_model, team1, team2)
+        if result == 'draw':
+            ranking.loc[[team1, team2], 'points'] += 1
+            ranking.loc[[team1, team2], 'goals'] += score[0]
+        else:
+            ranking.loc[result, 'points'] += 3
+            ranking.loc[result, 'goals'] += score[0]
+            ranking.loc[looser, 'goals'] += score[1]
+            ranking.loc[result, 'diff'] += score[0]-score[1]
+            ranking.loc[looser, 'diff'] -= score[0]-score[1]
+            
+    return ranking.sort_values(by=['points','diff','goals'], ascending=False)
+
+groups_ranking = []
+for group in groups:
+    groups_ranking.append(get_group_result(model, group))
+
+
+#Jogos da fase mata-mata
+
+def get_final_result(foot_model, groups_result):
+    round_of_16 = []
+    quarter_finals = []
+    semi_finals = []
+    
+    #Simulando rodada 16
+    for i in range(0, 8, 2):
+        round_of_16.append(get_match_result(foot_model, groups_result[i].index[0], groups_result[i+1].index[1], elimination=True))
+        round_of_16.append(get_match_result(foot_model, groups_result[i].index[1], groups_result[i+1].index[0], elimination=True))
+    
+    #Simulando quartas de final
+    quarter_finals.append(get_match_result(foot_model, round_of_16[0][0], round_of_16[2][0], elimination=True))
+    quarter_finals.append(get_match_result(foot_model, round_of_16[1][0], round_of_16[3][0], elimination=True))
+    quarter_finals.append(get_match_result(foot_model, round_of_16[4][0], round_of_16[6][0], elimination=True))
+    quarter_finals.append(get_match_result(foot_model, round_of_16[5][0], round_of_16[7][0], elimination=True))
+    
+    #Simulando semi-final
+    semi_finals.append(get_match_result(foot_model, quarter_finals[0][0], quarter_finals[2][0], elimination=True))
+    semi_finals.append(get_match_result(foot_model, quarter_finals[1][0], quarter_finals[3][0], elimination=True))
+    
+    #Simulando disputa de terceiro lugar
+    little_final = get_match_result(foot_model, semi_finals[0][1], semi_finals[1][1], elimination=True)
+    
+    #Simulando final
+    final = get_match_result(foot_model, semi_finals[0][0], semi_finals[1][0], elimination=True)
+    
+    return round_of_16, quarter_finals, semi_finals, little_final, final
+
+round_of_16, quarter_finals, semi_finals, little_final, final = get_final_result(model, groups_ranking)
+
+fig = plt.figure(figsize = (40,45))
+img = mpimg.imread('tabela.png')
+plt.imshow(img)
+plt.axis('off')
+
+def text_match(x, y, match, final=False):
+    col_win, col_loose = ('green', 'red') if (not final) else ('gold', 'silver')
+    plt.text(x, y, match[0], fontsize=23, color=col_win, weight='bold')
+    plt.text(x+120, y+1, match[2][0], fontsize=38, color=col_win, weight='bold')
+    plt.text(x, y+50, match[1], fontsize=23, color=col_loose, weight='bold')
+    plt.text(x+120, y+51, match[2][1], fontsize=38, color=col_loose, weight='bold')
+
+
+round_of_16_xy = [(40,110),(898,110),(40,280),(898,280),(40,430),(898,430),(40,600),(898,600)]
+quarter_finals_xy = [(212,198),(726,198),(212,518),(726,518)]
+semi_finals_xy = [(378,365),(560,365)]
+x_little_final, y_little_final = 560, 576
+x_final, y_final = 469, 157
+
+
+for (x, y), match in zip(round_of_16_xy, round_of_16):
+     text_match(x, y, match)
+for (x, y), match in zip(quarter_finals_xy, quarter_finals):
+        text_match(x, y, match)
+for (x, y), match in zip(semi_finals_xy, semi_finals):
+        text_match(x, y, match)
+    
+text_match(x_little_final, y_little_final, little_final)
+text_match(x_final, y_final, final, final=True)
+
+   
